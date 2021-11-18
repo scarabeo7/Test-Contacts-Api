db1d9f681aa19f0b47837305f1048b4255bd1225
Chizim Chinuru <chizim.chinuru@capgemini.com>
2021-11-18 16:26:04 +0000

    added search function

--- server.js
+++ server.js
@@ -2,16 +2,23 @@ const express = require("express");
 
 const app = express();
 
-
 //Use this array as your (in-memory) data store.
-const contacts = require("./contacts.json");
+const { contacts } = require("./contacts.json");
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.send(contacts);
+});
+
+app.get("/find", (req, res) => {
+  const inputEmail = req.query.email;
+  console.log(inputEmail)
+  const filteredEmail = contacts.find((entry) => entry.email === inputEmail);
+  console.log(filteredEmail)
+  res.send(filteredEmail);
 });
 
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
