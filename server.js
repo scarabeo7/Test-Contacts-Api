5f35e07581f83b3775ba2c26720f50ca836ed0ac
Chizim Chinuru <chizim.chinuru@capgemini.com>
2021-11-25 11:26:51 +0000

    using filter again

--- server.js
+++ server.js
@@ -9,35 +9,35 @@ app.get("/", (req, res) => {
   res.send(contacts);
 });
 
-app.get("/find", (req, res) => {
-  const inputTerm = req.query.entry;
-  const filteredTerm = contacts.find(
-    (entry) =>
-      entry.email.toLowerCase() === inputTerm.toLowerCase() ||
-      entry.name.toLowerCase() === inputTerm.toLowerCase()
-  );
-  if (filteredTerm) {
-    res.send(filteredTerm);
-  } else {
-    res.status(404);
-  }
-});
-
 // app.get("/find", (req, res) => {
 //   const inputTerm = req.query.entry;
-//   const filteredTerm = contacts.filter((entry) => {
-//     return (
-//       entry.email.toLowerCase().includes(inputTerm.toLowerCase()) ||
-//       entry.name.toLowerCase().includes(inputTerm.toLowerCase())
-//     );
-//   });
-
+//   const filteredTerm = contacts.find(
+//     (entry) =>
+//       entry.email.toLowerCase() === inputTerm.toLowerCase() ||
+//       entry.name.toLowerCase() === inputTerm.toLowerCase()
+//   );
 //   if (filteredTerm) {
 //     res.send(filteredTerm);
 //   } else {
 //     res.status(404);
 //   }
 // });
+
+app.get("/find", (req, res) => {
+  const inputTerm = req.query.entry;
+  const filteredTerm = contacts.filter((entry) => {
+    return (
+      entry.email.toLowerCase().includes(inputTerm.toLowerCase()) ||
+      entry.name.toLowerCase().includes(inputTerm.toLowerCase())
+    );
+  });
+
+  if (filteredTerm) {
+    res.send(filteredTerm);
+  } else {
+    res.status(404);
+  }
+});
 
 // TODO add your routes and helper functions here
 
