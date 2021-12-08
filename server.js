d891894459bd622545f749c4445a9aec8f7152e3
Chizim Chinuru <chizim.chinuru@capgemini.com>
2021-12-08 16:59:02 +0000

    changed api schema

--- server.js
+++ server.js
@@ -9,35 +9,36 @@ app.get("/", (req, res) => {
   res.send(contacts);
 });
 
+app.get("/:find", (req, res) => {
+  const getParam = req.params.find;
+  const filteredParam = contacts.filter(
+    (entry) =>
+      entry.email.toLowerCase() === getParam.toLowerCase() ||
+      entry.name.toLowerCase() === getParam.toLowerCase()
+  );
+
+  filteredParam
+    ? res.send(filteredParam)
+    : res
+        .status(404)
+        .json({ message: `${getParam} not found, please try again` });
+});
+
 // app.get("/find", (req, res) => {
 //   const inputTerm = req.query.entry;
-//   const filteredTerm = contacts.find(
-//     (entry) =>
-//       entry.email.toLowerCase() === inputTerm.toLowerCase() ||
-//       entry.name.toLowerCase() === inputTerm.toLowerCase()
-//   );
+//   const filteredTerm = contacts.filter((entry) => {
+//     return (
+//       entry.email.toLowerCase().includes(inputTerm.toLowerCase()) ||
+//       entry.name.toLowerCase().includes(inputTerm.toLowerCase())
+//     );
+//   });
+
 //   if (filteredTerm) {
 //     res.send(filteredTerm);
 //   } else {
 //     res.status(404);
 //   }
 // });
-
-app.get("/find", (req, res) => {
-  const inputTerm = req.query.entry;
-  const filteredTerm = contacts.filter((entry) => {
-    return (
-      entry.email.toLowerCase().includes(inputTerm.toLowerCase()) ||
-      entry.name.toLowerCase().includes(inputTerm.toLowerCase())
-    );
-  });
-
-  if (filteredTerm) {
-    res.send(filteredTerm);
-  } else {
-    res.status(404);
-  }
-});
 
 // TODO add your routes and helper functions here
 
