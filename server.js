b5c6b56a369dd334b3649600de67bcab726085f4
Chizim Chinuru <chizim.chinuru@capgemini.com>
2022-02-17 15:31:06 +0000

    updated contacts file as per required

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 
 const app = express();
 
-//Use this array as your (in-memory) data store.
 const { contacts } = require("./contacts.json");
 
 app.get("/", (req, res) => {
@@ -10,11 +9,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/:find", (req, res) => {
-  const getParam = req.params.find;
+  const getParam = req.params.find.replace(/\s/g, "").toLowerCase();
   const filteredParam = contacts.filter(
     (entry) =>
-      entry.email.toLowerCase() === getParam.toLowerCase() ||
-      entry.name.toLowerCase() === getParam.toLowerCase()
+      entry.accountName.replace(/\s/g, "").toLowerCase() === getParam ||
+      entry.postcode.replace(/\s/g, "").toLowerCase() === getParam
   );
 
   filteredParam
@@ -23,24 +22,6 @@ app.get("/:find", (req, res) => {
         .status(404)
         .json({ message: `${getParam} not found, please try again` });
 });
-
-// app.get("/find", (req, res) => {
-//   const inputTerm = req.query.entry;
-//   const filteredTerm = contacts.filter((entry) => {
-//     return (
-//       entry.email.toLowerCase().includes(inputTerm.toLowerCase()) ||
-//       entry.name.toLowerCase().includes(inputTerm.toLowerCase())
-//     );
-//   });
-
-//   if (filteredTerm) {
-//     res.send(filteredTerm);
-//   } else {
-//     res.status(404);
-//   }
-// });
-
-// TODO add your routes and helper functions here
 
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
